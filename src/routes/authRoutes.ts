0759a02a72715e42c42063370b2cb350bf292a02
bante27 <mitikubantalem@gmail.com>
2026-08-27 23:51:39 -0900

    delet router configur

--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controllers/authController';
-
-const router = Router();
-
-router.post('/register-admin', controller.registerAdmin);
-router.post('/login', controller.login);
-
-export default router;
